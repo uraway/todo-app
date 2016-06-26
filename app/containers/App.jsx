56b081256c0814396efe4421708e00b61a6f86ef
uraway <masato.u0107@gmail.com>
2016-06-26 18:56:38 +0900

    add signup and login button

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import REVISION from '../../config/revision';
 
+import FlatButton from 'material-ui/FlatButton';
+
 export default class App extends Component {
   render() {
     return (
       <div>
-        <p>Front-end application for Todos.
-          <br />
+        <FlatButton
+          label="signup"
+        />
+        <FlatButton
+          label="login"
+        />
+        <br />
+        <br />
+        <h1>Hello, your Todos!</h1>
+        <br />
+        <p>
           Revision is {REVISION}
         </p>
       </div>
