5f21666990bfbde21ba30f969ea6f0f7e28d838a
uraway <masato.u0107@gmail.com>
2016-06-26 21:51:13 +0900

    add router to App

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,17 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import REVISION from '../../config/revision';
 
 import FlatButton from 'material-ui/FlatButton';
 
 export default class App extends Component {
+  static contextTypes = {
+    router: PropTypes.object.isRequired,
+  };
   render() {
+    const { router } = this.context;
     return (
       <div>
         <FlatButton
           label="signup"
+          onClick={() => router.push('/signup')}
         />
         <FlatButton
           label="login"
+          onClick={() => router.push('/login')}
         />
         <br />
         <br />
