edc7e4cad44ad572fd3299239fd9e54cc061a59b
uraway <masato.u0107@gmail.com>
2016-06-29 10:45:35 +0900

    remove login signup link from App

--- app/containers/App.jsx
+++ app/containers/App.jsx
@@ -1,24 +1,13 @@
 import React, { Component, PropTypes } from 'react';
 import REVISION from '../../config/revision';
 
-import FlatButton from 'material-ui/FlatButton';
-
 export default class App extends Component {
   static contextTypes = {
     router: PropTypes.object.isRequired,
   };
   render() {
-    const { router } = this.context;
     return (
       <div>
-        <FlatButton
-          label="signup"
-          onClick={() => router.push('/signup')}
-        />
-        <FlatButton
-          label="login"
-          onClick={() => router.push('/login')}
-        />
         <br />
         <br />
         <h1>Hello, your Todos!</h1>
