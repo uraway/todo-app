14f11d38feda8d25408ec562e8410971b118825e
uraway <masato.u0107@gmail.com>
2016-06-29 10:11:55 +0900

    SignupActions

--- app/actions/SignupActions.js
+++ app/actions/SignupActions.js
@@ -1,7 +1,7 @@
 import apiCall from '../utils/apiCall';
 import * as actionTypes from '../constants/SignupConstants';
 
-export function signup({ data, authAgent, router, backPathStore }) {
+export function signup({ data, router }) {
   return (dispatch) => {
     dispatch({
       type: actionTypes.SIGNUP_REQUESTED,
@@ -9,31 +9,24 @@ export function signup({ data, authAgent, router, backPathStore }) {
 
     return apiCall({
       method: 'POST',
-      path: '/signup',
+      path: '/users',
       data,
     })
     .then((res) => {
-      const { email, access_token } = res.data.data.attributes;
-
-      authAgent.login(email, access_token, {
-        sessionOnly: false,
-        cb: () => {
-          dispatch({
-            type: actionTypes.SIGNUP_SUCCEED,
-            user: email,
-          });
-          router.push(backPathStore);
-        },
+      const { data } = res.data;
+      console.log(data);
+      dispatch({
+        type: actionTypes.SIGNUP_SUCCEED,
       });
+      router.push('/app');
     })
     .catch((res) => {
       if (res.data === undefined) return;
-
       dispatch({
         type: actionTypes.SIGNUP_FAILED,
         errors: {
           code: res.status,
-          data: res.data.error,
+          data: res.data.error ? res.data.error : res.statusText,
         },
       });
     });
